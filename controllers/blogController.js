edfc17631f46370cb16381555fde652c9680e08a
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-01-16 13:35:24 +0100

    feat: funzioni per il controller e importate nel router ;)

---
+++ controllers/blogController.js
@@ -0,0 +1,47 @@
+import connection from "../connections/db-connection.js";
+
+
+
+
+
+//index
+function index (req, res) {
+
+}
+
+//show
+function show (req, res) {
+
+}
+
+//store 
+function store (req, res) {
+
+}
+
+//update
+function update(req, res) {
+
+}
+
+//modify
+function modify(req, res) {
+
+}
+
+//destroy 
+function destroy(req, res) {
+
+}
+
+
+const controller = {
+    index,
+    show,
+    store,
+    update,
+    modify,
+    destroy,   
+};
+
+export default controller
--- routers/blogRouter.js
+++ routers/blogRouter.js
@@ -1,17 +1,29 @@
 import express from "express"
-
+import controller from "../controllers/blogController.js"
 const router = express.Router() 
 
 //index 
+router.get("/", controller.index)
+
 
 //show 
+router.get("/:id", controller.show)
+
 
 //store 
+router.post("/", controller.store)
+
 
 //update
+router.put("/:id", controller.update)
+
 
 //modify
+router.patch("/:id", controller.modify)
+
 
 //destroy
+router.delete("/:id", controller.destroy)
+
 
 export default router
