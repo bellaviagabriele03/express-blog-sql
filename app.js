e7587ad0f4f34c8e2cbd1580150158efc06905fd
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-01-16 13:08:37 +0100

    feat: app inizializzata

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express" 
+
+const app = express();
+const port = 3000; 
+app.use(express.json());
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server listen on port: ${port}`);
+    
+})
