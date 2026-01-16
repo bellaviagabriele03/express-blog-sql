8114507a2977b2cee79c9a3db50643ac72a018ec
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-01-16 13:22:50 +0100

    feat: router wip

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from "express" 
 import connection from "./connections/db-connection.js";
+import blogrouter from "./routers/blogRouter.js";
 const app = express();
 const port = 3000; 
 app.use(express.json());
 
-
+app.use("/posts", blogrouter)
 
 
 
---
+++ routers/blogRouter.js
@@ -0,0 +1,17 @@
+import express from "express"
+
+const router = express.Router() 
+
+//index 
+
+//show 
+
+//store 
+
+//update
+
+//modify
+
+//destroy
+
+export default router
